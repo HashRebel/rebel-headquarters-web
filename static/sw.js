34408e780e2255ace5ae4daa7158829ae33859a7
Brian Henze <brian.henze@hashrebel.com>
2018-06-22 12:47:35 -0700

    tes

--- static/sw.js
+++ static/sw.js
@@ -1,35 +1,35 @@
 importScripts('/_nuxt/workbox.3de3418b.js')
 
 const workboxSW = new self.WorkboxSW({
-  "cacheId": "order-pwa-poc",
+  "cacheId": "rebel-headquarters-sandbox-test",
   "clientsClaim": true,
   "directoryIndex": "/"
 })
 
 workboxSW.precache([
   {
-    "url": "/_nuxt/app.41a0e116f7a119c0d828.js",
-    "revision": "6cc564128669c94d037bd0315cbcff20"
+    "url": "/_nuxt/app.8862f17b42c3d1211d27.js",
+    "revision": "c47d74afc3c8cd6a6b5e3dbbe05aef0e"
   },
   {
-    "url": "/_nuxt/layouts/default.6de1868c603fcd6679d6.js",
-    "revision": "c6433ebf49eb782fdda87206a82cfb4f"
+    "url": "/_nuxt/layouts/default.782fb50f53236c103ab3.js",
+    "revision": "aed83f19e214f77e43e9ede90179af39"
   },
   {
-    "url": "/_nuxt/manifest.120393d3e37b7cf91096.js",
-    "revision": "fd40d729a3f31772ee6f372b9a3bdcae"
+    "url": "/_nuxt/manifest.9f8f094e10c221a6a63e.js",
+    "revision": "8758159de7747ea4fc15f1098584a935"
   },
   {
-    "url": "/_nuxt/pages/catolog.d3dc9f852c8ce9f0c545.js",
-    "revision": "c2cb982f6108a709eba37fcef23de137"
+    "url": "/_nuxt/pages/index.5316e573cf490d76b7eb.js",
+    "revision": "70dcd67191a0fde8107313e23f158cf0"
   },
   {
-    "url": "/_nuxt/pages/index.117bafdc737a0b64748e.js",
-    "revision": "c65e751326d6970e28416871c13eff8c"
+    "url": "/_nuxt/pages/resume.1c2ea9ba6c66a9b98413.js",
+    "revision": "26dfe3afff23cfb6ee96452e4246d322"
   },
   {
-    "url": "/_nuxt/vendor.14643f6166d3a87f9ab2.js",
-    "revision": "0ac2c27b1bd64b1a9ff86f4c35eddac7"
+    "url": "/_nuxt/vendor.9af420cceaa6eb552a5e.js",
+    "revision": "1c52e27ddcec242663fc357e0b735d54"
   }
 ])
 
